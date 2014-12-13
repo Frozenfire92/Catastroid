b79ff6a2b2e6515e33466b352af75a11123072bd
rainbee2214 <rainbee2214@gmail.com>
2014-12-12 20:39:32 -0400

    Add rocks

---
+++ Catastroid/Assets/Scripts/TravelTowards.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TravelTowards : MonoBehaviour 
+{
+    public GameObject target;
+    Vector3 position;
+
+	void Start () 
+    {
+	
+	}
+	
+	void Update () 
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, Time.time);
+        //position = transform.position;
+        //transform.position = Vector3.Lerp(Vector3.zero, target.transform.position, Time.time);
+	}
+}
