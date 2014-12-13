5fc161b13e4a33ea754f3d3eb2edfd6a3ff985a7
Joel Kuntz <joel.kuntz.92@gmail.com>
2014-12-12 20:34:06 -0400

    Update game over scene

---
+++ Catastroid/Assets/LevelLoader.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelLoader : MonoBehaviour 
+{
+	public string level;
+	public float delay;
+
+	void Start () 
+	{
+	
+	}
+
+	void Update () 
+	{
+		if (Time.time > delay) Application.LoadLevel(level);
+	}
+}
